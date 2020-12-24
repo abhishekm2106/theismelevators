fdfc2af49ad383adf31cd5b2a74f899cef0542be
Abhishek <abhishekm2106@gmail.com>
2020-12-24 18:55:00 +0530

    first

---
+++ javaScript/index.js
@@ -0,0 +1,88 @@
+var burger = document.querySelector(".burger")
+var navbar = document.querySelector(".nav-bar")
+
+var lines = document.querySelectorAll(".line")
+
+burger.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    // lines[1].style.display = "none";
+})
+
+
+
+// ----------------------------------------------------------------------------------
+
+
+
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true; // Auto scroll
+const intervalTime = 6000;
+let slideInterval;
+
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+// Button events
+next.addEventListener('click', e => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+prev.addEventListener('click', e => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+// Auto slide
+if (auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
+
+
+
+
+
+window.addEventListener('scroll',function(){
+    const slider = document.querySelector('.slider');
+    let scrollp = window.pageYOffset;
+    
+    slider.style.transform = 'translateY(' + scrollp * .65 + 'px)';
+})
